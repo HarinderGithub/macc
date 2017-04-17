1425a9a78757870c11549ca11990d0b992997d60
Harinder Kaur <snazyahuja11@gmail.com>
2017-04-17 15:03:55 -0400

    Removed Redundant urls

--- infohub/urls.py
+++ infohub/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns(
     url(r'^', include('webhub.urls')),
     url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^malaria/', include('malaria_web.urls', namespace='malaria')),
-    url(r'^webhub/', include('webhub.urls', namespace='webhub')),
+    #url(r'^webhub/', include('webhub.urls', namespace='webhub')),
     url(r'^signup/', include('signup.urls', namespace='signup')),
     url(r'^profiles/', include('profiles.urls', namespace='profiles')),
     url(r'^pcsa/', include('pcsa.urls', namespace='pcsa')),
